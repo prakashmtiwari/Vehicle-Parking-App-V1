5bdd78f449d99cc3af3203e0298b0db7adc301b9
Prakash Tiwari <prakashh.m.tiwari@gmail.com>
2026-06-18 18:24:47 +0530

    removed default port

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from vpa import app
 
 # Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port="5001")
+    app.run(debug=True, host="0.0.0.0")
